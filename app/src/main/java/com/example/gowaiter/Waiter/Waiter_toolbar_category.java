ebb7f817b9412ae80d4049ed1d8346f19bd0b257
tonia <toniagiannoulaki@gmai.com>
2024-10-09 22:26:14 +0300

    Waiter_toolbar_category added

---
+++ app/src/main/java/com/example/gowaiter/Waiter/Waiter_toolbar_category.java
@@ -0,0 +1,16 @@
+package com.example.gowaiter.Waiter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.gowaiter.R;
+
+public class Waiter_toolbar_category extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_waiter_toolbar_category);
+    }
+}
