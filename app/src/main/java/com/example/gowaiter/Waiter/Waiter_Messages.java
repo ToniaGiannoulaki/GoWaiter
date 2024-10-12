4b0e7a17aff73fac90f9f0712df0b005d31546c7
tonia <toniagiannoulaki@gmai.com>
2024-10-12 21:36:23 +0300

    Waiter, Barista and Chef messages added. Waiter change order added

---
+++ app/src/main/java/com/example/gowaiter/Waiter/Waiter_Messages.java
@@ -0,0 +1,16 @@
+package com.example.gowaiter.Waiter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.gowaiter.R;
+
+public class Waiter_Messages extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_waiter_messages);
+    }
+}
