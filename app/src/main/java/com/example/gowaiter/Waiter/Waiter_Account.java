23d7757e26b81bfac02241c83cbbb6524eae7cc6
tonia <toniagiannoulaki@gmai.com>
2024-12-07 19:54:50 +0200

    Change name activity from change orders to statistcs

--- app/src/main/java/com/example/gowaiter/Waiter/Waiter_Account.java
+++ app/src/main/java/com/example/gowaiter/Waiter/Waiter_Account.java
@@ -10,21 +10,21 @@ import com.example.gowaiter.R;
 
 public class Waiter_Account extends AppCompatActivity {
 
-    CardView takeOrder, changeOrder, notifications, account_settings, payments, supplies;
+    CardView takeOrder, statistics, notifications, account_settings, payments, supplies;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_waiter_account);
 
         takeOrder = findViewById(R.id.card_view_orders_waiter);
-        changeOrder = findViewById(R.id.card_view_change_order);
-        notifications = findViewById(R.id.card_view_statistics_waiter);
+        statistics = findViewById(R.id.card_view_statistics_waiter);
+        notifications = findViewById(R.id.card_view_order_notifications);
         account_settings = findViewById(R.id.card_view_account_settings_waiter);
         payments = findViewById(R.id.card_view_payments_waiter);
         supplies = findViewById(R.id.card_view_supplies_waiter);
 
         takeOrder.setOnClickListener(v -> startActivity(new Intent(Waiter_Account.this, Waiter_Take_Order.class)));
-        changeOrder.setOnClickListener(v -> startActivity(new Intent(Waiter_Account.this, Waiter_Change_Order.class)));
+        statistics.setOnClickListener(v -> startActivity(new Intent(Waiter_Account.this, Waiter_Statistics.class)));
         notifications.setOnClickListener(v -> startActivity(new Intent(Waiter_Account.this, Waiter_Notifications.class)));
         account_settings.setOnClickListener(v -> startActivity(new Intent(Waiter_Account.this, Waiter_Account_Settings.class)));
         payments.setOnClickListener(v -> {startActivity(new Intent(Waiter_Account.this, Waiter_Payments.class));});
